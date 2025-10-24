ffd1ff6308dce55e751e3c1145a507e49b54dfa9
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-25 02:12:29 +0530

    change9

--- app/user/resetPassword/page.tsx
+++ app/user/resetPassword/page.tsx
@@ -1,8 +1,16 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  const params = useSearchParams();
+  const token = params.get("token");
+  return (
+    <div>
+      <h1>Reset Password</h1>
+      <p>Token: {token}</p>
+    </div>
+  );
 };
 
 export default Page;
