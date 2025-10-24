ad6c644c091b64bfcf616605e5906ce88bcd1cba
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-25 02:20:06 +0530

    change10

--- app/user/resetPassword/page.tsx
+++ app/user/resetPassword/page.tsx
@@ -1,16 +1,24 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import React from "react";
 
-const Page = () => {
-  const params = useSearchParams();
-  const token = params.get("token");
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+
+function ResetPasswordContent() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
   return (
-    <div>
+    <div className="p-4">
       <h1>Reset Password</h1>
       <p>Token: {token}</p>
     </div>
   );
-};
+}
 
-export default Page;
+export default function ResetPasswordPage() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ResetPasswordContent />
+    </Suspense>
+  );
+}
